4d5dfe9eacd31f1255fe223ef50f7d946cc887dd
Lf <lfsoftware13@gmail.com>
2017-01-05 02:34:59 +0800

    init repository with interfaces

---
+++ CppMonitor/Monitor/CommandMonitor/CommandBindEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanjingUniversity.CppMonitor.Monitor.CommandMonitor
+{
+    class CommandBindEvent : IBindEvent
+    {
+        void IBindEvent.RegisterEvent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
