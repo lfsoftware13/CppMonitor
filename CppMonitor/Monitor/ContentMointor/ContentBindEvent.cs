60cd929b8859d0509509ff37513f02f2220015ce
iaxax <1483809252@qq.com>
2017-01-07 20:21:53 +0800

    初始化DTE以及相应的监听事件绑定

--- CppMonitor/Monitor/ContentMointor/ContentBindEvent.cs
+++ CppMonitor/Monitor/ContentMointor/ContentBindEvent.cs
@@ -3,14 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using EnvDTE80;
+using EnvDTE;
 
 namespace NanjingUniversity.CppMonitor.Monitor.ContentMointor
 {
     class ContentBindEvent : IBindEvent
     {
+        private DTE2 Dte2;
+
+        private Events DteEvents;
+
+        private TextEditorEvents TextEvents;
+
+        public ContentBindEvent()
+        {
+            Dte2 = (DTE2)Microsoft.VisualStudio.Shell.Package.
+                GetGlobalService(typeof(Microsoft.VisualStudio.Shell.Interop.SDTE));
+            DteEvents = Dte2.Events;
+            TextEvents = DteEvents.TextEditorEvents;
+        }
+         
         void IBindEvent.RegisterEvent()
         {
-            throw new NotImplementedException();
+            TextEvents.LineChanged += OnTextChange;
+        }
+
+        private void OnTextChange(TextPoint StartPoint, TextPoint EndPoint, int Hint)
+        {
+            
         }
     }
 }
