c731550ead6a503e4ed923146bc3fa453ed031a8
Lf <lfsoftware13@gmail.com>
2017-01-10 06:12:52 +0800

    新增build信息记录对象

---
+++ CppMonitor/Monitor/BuildMonitor/BO/BuildInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanjingUniversity.CppMonitor.Monitor.BuildMonitor.BO
+{
+    class BuildInfo
+    {
+        public string BuildStartTime
+        {
+            get
+            {
+                return BuildStartTime;
+            }
+            set
+            {
+                BuildStartTime=value;
+            }
+        }
+
+        public string BuildEndTime
+        {
+            get
+            {
+                return BuildEndTime;
+            }
+            set
+            {
+                BuildEndTime = value;
+            }
+        }
+
+        public string Content
+        {
+            get
+            {
+                return Content;
+            }
+            set
+            {
+                Content = value;
+            }
+        }
+
+        public List<BuildProjectInfo> Projects
+        {
+            get
+            {
+                return Projects;
+            }
+            set
+            {
+                Projects = value;
+            }
+        }
+
+    }
+}
---
+++ CppMonitor/Monitor/BuildMonitor/BO/BuildProjectInfo.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NanjingUniversity.CppMonitor.Monitor.BuildMonitor.BO
+{
+    class BuildProjectInfo
+    {
+        public string BuildProjectStartTime
+        {
+            get
+            {
+                return BuildProjectStartTime;
+            }
+            set
+            {
+                BuildProjectStartTime = value;
+            }
+        }
+
+        public string BuildProjectEndTime
+        {
+            get
+            {
+                return BuildProjectEndTime;
+            }
+            set
+            {
+                BuildProjectEndTime = value;
+            }
+        }
+
+        public string ConfigurationName
+        {
+            get
+            {
+                return ConfigurationName;
+            }
+            set
+            {
+                ConfigurationName = value;
+            }
+        }
+
+        public string ConfigurationType
+        {
+            get
+            {
+                return ConfigurationType;
+            }
+            set
+            {
+                ConfigurationType = value;
+            }
+        }
+
+        public string ProjectName
+        {
+            get
+            {
+                return ProjectName;
+            }
+            set
+            {
+                ProjectName = value;
+            }
+        }
+
+        public string CommandArguments
+        {
+            get
+            {
+                return CommandArguments;
+            }
+            set
+            {
+                CommandArguments = value;
+            }
+
+        }
+
+        public string RunCommand
+        {
+            get
+            {
+                return RunCommand;
+            }
+            set
+            {
+                RunCommand = value;
+            }
+        }
+
+        public string BuildLogFile
+        {
+            get
+            {
+                return BuildLogFile;
+            }
+            set
+            {
+                BuildLogFile = value;
+            }
+        }
+
+        public string BuildLogContent
+        {
+            get
+            {
+                return BuildLogContent;
+            }
+            set
+            {
+                BuildLogContent = value;
+            }
+        }
+
+        public string CompilerCommand
+        {
+            get
+            {
+                return CompilerCommand;
+            }
+            set
+            {
+                CompilerCommand = value;
+            }
+        }
+
+        public string LinkCommand
+        {
+            get
+            {
+                return LinkCommand;
+            }
+            set
+            {
+                LinkCommand = value;
+            }
+        }
+
+
+    }
+}
