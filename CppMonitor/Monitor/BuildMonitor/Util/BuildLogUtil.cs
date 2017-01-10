16a2dbe65f702d8beea4fd6fa6bce8856c8fe31d
Lf <lfsoftware13@gmail.com>
2017-01-11 01:07:12 +0800

    新增日志工具类

---
+++ CppMonitor/Monitor/BuildMonitor/Util/BuildLogUtil.cs
@@ -0,0 +1,25 @@
+﻿using NanjingUniversity.CppMonitor.Monitor.BuildMonitor.BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NanjingUniversity.CppMonitor.Monitor.BuildMonitor.Util
+{
+    class BuildLogUtil
+    {
+        public static Boolean LogBuildInfo(BuildInfo info)
+        {
+            return true;
+        }
+
+        public static Boolean LogMessage(string Message)
+        {
+            return true;
+        }
+
+
+    }
+}
